687cea66fc2e7a1c14b6648086ea7425aca12e68
Dinorush <onparnorthstar@gmail.com>
2024-07-12 00:08:45 -0400

    Version bump

--- ProgressionGear/EntryPoint.cs
+++ ProgressionGear/EntryPoint.cs
@@ -8,7 +8,7 @@ using ProgressionGear.Patches;
 
 namespace ProgressionGear;
 
-[BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.1.2")]
+[BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.1.3")]
 [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MTFOUtil.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(LocalProgressionWrapper.GUID, BepInDependency.DependencyFlags.SoftDependency)]
